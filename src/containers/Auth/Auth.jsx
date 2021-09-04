9a1ec870e430742743f03c184d86a1b9b56d7d97
AlexDyatlov <aleks78.gooogle@gmail.com>
2021-09-05 00:33:17 +0300

    added form component

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import classes from './Auth.module.scss';
+
+import Input from '../../components/Ui/Input/Input';
+import Button from '../../components/Ui/Button/Button';
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  };
+
+  registerHandler = () => {
+
+  };
+  
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form className={classes.Auth__Form} onSubmit={this.submitHandler}>
+            <Input label="Email" />
+            <Input 
+              label="Пароль"
+              errorMessage={' '}
+            />
+
+            <Button 
+              type="success" 
+              onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+
+            <Button 
+              type="primary" 
+              onClick={this.registerHandler}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
-}
+};
